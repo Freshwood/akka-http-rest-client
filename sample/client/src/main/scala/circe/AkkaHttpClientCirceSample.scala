a385c28931baa4b8f353f97714259964eb781749
Tobias Frischholz <tfrischholz@kabelmail.de>
2018-08-13 16:09:31 +0200

    Added a package for each json framework

--- sample/client/src/main/scala/circe/AkkaHttpClientCirceSample.scala
+++ sample/client/src/main/scala/circe/AkkaHttpClientCirceSample.scala
@@ -1,30 +1,23 @@
+package circe
+
 import akka.actor.ActorSystem
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.stream.{ActorMaterializer, Materializer}
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+import io.circe.generic.auto._
 import net.softler.client.ClientRequest
-import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 import scala.concurrent.{ExecutionContext, Future}
-
-object JsonModel {
-  case class GithubUser(login: String)
-}
-
-sealed trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  import JsonModel._
-  implicit val githubUserFormat: RootJsonFormat[GithubUser] = jsonFormat1(GithubUser)
-}
 
 /**
   * Sample shows how to fetch json information in an very simple fashion
   */
-object AkkaHttpClientJsonSample extends App with JsonSupport {
-
-  import JsonModel._
+object AkkaHttpClientCirceSample extends App with FailFastCirceSupport {
 
   implicit lazy val system: ActorSystem = ActorSystem()
   implicit lazy val materializer: Materializer = ActorMaterializer()
   implicit lazy val executionContext: ExecutionContext = system.dispatcher
+
+  case class GithubUser(login: String)
 
   val clientRequest: Future[GithubUser] = ClientRequest("https://api.github.com/users/Freshwood")
     .get[GithubUser]
